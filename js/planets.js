fb67a1297f7b5930eb7b9cfd07f0c278e04dbd56
Michael013 <159182494+Michael000013@users.noreply.github.com>
2026-03-10 16:13:05 +0000

    Basic Given Template for MidTerm

---
+++ js/planets.js
@@ -0,0 +1,20 @@
+// API: 
+// https://api.visibleplanets.dev/v3?latitude=12&longitude=-981
+
+// Card Template:
+    // <div class="entry" id="">
+    // 	<figure>
+    //         <img src="" alt="" />
+    // 	<figcaption></figcaption>
+    // 	</figure>
+    // </div>
+
+$(document).ready(function() {
+    var bootstrapButton = $.fn.button.noConflict(); 
+	$.fn.bootstrapBtn = bootstrapButton;
+
+    // Write your code here
+
+    
+
+});
